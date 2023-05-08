b8b0279c1ee9261b9850f68b84e02836609fa015
ank010 <ank010@ucsd.edu>
2023-05-07 19:26:10 -0700

    Fix img source links

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -19,8 +19,8 @@ function init() {
   
 
   function updateImageSound(){
-    document.querySelector("img").src = `./assets/images/${selector.value}.svg`;
-    hornAudio.src = `./assets/audio/${selector.value}.mp3`
+    document.querySelector("img").src = `assets/images/${selector.value}.svg`;
+    hornAudio.src = `assets/audio/${selector.value}.mp3`
     
   }
 
@@ -34,16 +34,16 @@ function init() {
   function changeVolume(){
       let volumeImg = document.getElementById("volume-controls").querySelector("img");
       if(volume.value < 33 && volume.value > 1){
-        volumeImg.src = "./assets/icons/volume-level-1.svg";
+        volumeImg.src = "assets/icons/volume-level-1.svg";
         
       }else if(volume.value > 32 && volume.value < 64){
-        volumeImg.src = "./assets/icons/volume-level-2.svg";
+        volumeImg.src = "assets/icons/volume-level-2.svg";
         
       }else if(volume.value > 63){
-        volumeImg.src = "./assets/icons/volume-level-3.svg";
+        volumeImg.src = "assets/icons/volume-level-3.svg";
         
       }else{
-        volumeImg.src = "./assets/icons/volume-level-0.svg";
+        volumeImg.src = "assets/icons/volume-level-0.svg";
       }
       hornAudio.volume = volume.value/100;
   }
